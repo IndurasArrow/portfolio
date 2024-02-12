84a47c0d169c087692776645bfbdeb6afab1fb25
divyansh maewal <maewal.divyansh@gmail.com>
2024-02-12 19:02:09 +0530

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen w-full flex-col items-center gap-2 justify-center p-24">
+      <h1 className=" text-5xl font-bold">Divyansh Maewal</h1>
+      <h2 className="text-2xl italic">UX/Ui Designer</h2>
+      <p>Under consturction</p>
+    </main>
+  );
+}
